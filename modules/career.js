1ae048a155b886c0abf642b52473c65671d89a9b
Julius Herbert <juliush531@gmail.com>
2021-02-14 01:25:02 +0100

    redesign competence section

--- modules/career.js
+++ modules/career.js
@@ -1,9 +1,9 @@
 import {setWheelBehaviorFor } from "./utilities/wheelBehavior.js";
 export function careerFunctionalities() {
     let careerBgWrapper = document.querySelector('#career .section-content .bg-wrapper');
-    let accademicBgWrapper = document.querySelector('#accademic .section-content .bg-wrapper');
+    let academicBgWrapper = document.querySelector('#academic .section-content .bg-wrapper');
     let competencesBgWrapper = document.querySelector('#competences .section-content .bg-wrapper');
     setWheelBehaviorFor(careerBgWrapper);
-    setWheelBehaviorFor(accademicBgWrapper);
+    setWheelBehaviorFor(academicBgWrapper);
     setWheelBehaviorFor(competencesBgWrapper);
 }
