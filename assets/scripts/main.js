58bedb7f52ece67e16b5c1e936543f6ef4e1ce0c
juliush-dev <jusliush.dev@gmail.com>
2022-06-16 18:31:29 -0400

    undo redesign

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,16 +1,6 @@
-window.addEventListener("load", function () {
-  //everything is fully loaded, don't use me if you can use DOMContentLoaded
-  let menu = document.querySelector(".menu .material-icons");
-  let presentation = document.querySelector(".presentation");
-  let detailedView = document.querySelector(".detailed-view");
-  menu.addEventListener("click", (ev) => {
-    let presentationStyle = getComputedStyle(presentation);
-    if (presentationStyle.getPropertyValue("display") === "none") {
-      presentation.style.display = "grid";
-      detailedView.style.display = "none";
-    } else {
-      presentation.style.display = "none";
-      detailedView.style.display = "block";
-    }
-  });
-});
+import { navigationFunctionalities } from "../../modules/navigation.js";
+import { projectFunctionalities } from "../../modules/project.js";
+import { careerFunctionalities} from "../../modules/career.js";
+navigationFunctionalities();
+projectFunctionalities();
+careerFunctionalities();
